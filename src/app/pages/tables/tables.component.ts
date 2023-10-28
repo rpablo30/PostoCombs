b45bc198e9fdfdffa9cd8fe42c48af8833498c9f
Rodrigo Pablo <rpablo30@hotmail.com>
2023-10-28 14:11:35 -0300

    Separando os abastecimentos separado por bombas , ou mostrando todas as bombas.

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Abastecimento } from 'src/app/model/Abastecimeto.model';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator'; // Importe o MatPaginator
-
+import { MatPaginator } from '@angular/material/paginator';
 import { AbastecimentoService } from 'src/app/services/abastecimento.service';
+import { Abastecimento } from 'src/app/model/Abastecimeto.model';
 
 @Component({
   selector: 'app-tables',
@@ -11,19 +10,44 @@ import { AbastecimentoService } from 'src/app/services/abastecimento.service';
   styleUrls: ['./tables.component.scss']
 })
 export class TablesComponent implements OnInit {
-  displayedColumns: string[] = ['id','quantidadeLitros','valorAbastecido','bomba','combustivel','data'];
+  displayedColumns: string[] = ['id', 'quantidadeLitros', 'valorAbastecido', 'bomba', 'combustivel', 'data'];
   dataSource: MatTableDataSource<Abastecimento>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  abastecimentos: Abastecimento[]; 
+  abastecimentosOriginais: Abastecimento[]; // Uma cópia dos dados originais
+  selectedBomba: string = 'all';
+  selectedCombustivel: string = 'all';
+  showBombaSelect: boolean = false;
 
   constructor(
-    private abastecimentoService: AbastecimentoService,
-    private abastecimentoLocalService: AbastecimentoService
+    private abastecimentoService: AbastecimentoService
   ) {}
 
   ngOnInit() {
     this.abastecimentoService.getAbastecimentos().subscribe(abastecimentos => {
-      this.dataSource = new MatTableDataSource<Abastecimento>(abastecimentos);
+      this.abastecimentosOriginais = abastecimentos; // Manter uma cópia dos dados originais
+      this.abastecimentos = abastecimentos; // Inicialmente, ambos contêm os dados originais
+      this.dataSource = new MatTableDataSource<Abastecimento>(this.abastecimentos);
       this.dataSource.paginator = this.paginator;
     });
+  }
+
+  applyFilters() {
+    this.filterData();
+    this.dataSource = new MatTableDataSource<Abastecimento>(this.abastecimentos);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  filterData() {
+    if (this.selectedBomba === 'all' && this.selectedCombustivel === 'all') {
+      // Redefinir para os dados originais se todos os filtros estiverem em "all"
+      this.abastecimentos = this.abastecimentosOriginais.slice();
+    } else {
+      this.abastecimentos = this.abastecimentosOriginais.filter(abastecimento => {
+        const filterBomba = this.selectedBomba === 'all' || abastecimento.bomba === this.selectedBomba;
+        const filterCombustivel = this.selectedCombustivel === 'all' || abastecimento.combustivel === this.selectedCombustivel;
+        return filterBomba && filterCombustivel;
+      });
+    }
   }
 }
