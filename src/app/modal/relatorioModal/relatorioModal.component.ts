fcd1482d26d0507cd8acaa23f0b4f12ab7375a98
Rodrigo Pablo <rpablo30@hotmail.com>
2023-10-29 16:59:40 -0300

    Importado o date-fns para ajustes na data , feito ajustes no modal de relatório para configurar a exportação do pdf e excel.

--- src/app/modal/relatorioModal/relatorioModal.component.ts
+++ src/app/modal/relatorioModal/relatorioModal.component.ts
@@ -2,6 +2,9 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Abastecimento } from 'src/app/model/Abastecimeto.model';
 import { AbastecimentoService } from 'src/app/services/abastecimento.service';
+import jsPDF from 'jspdf';
+import * as XLSX from 'xlsx';
+import { MatSnackBar } from '@angular/material/snack-bar'; // Importe o MatSnackBar
 
 @Component({
   selector: 'app-relatorioModal',
@@ -9,8 +12,9 @@ import { AbastecimentoService } from 'src/app/services/abastecimento.service';
   styleUrls: ['./relatorioModal.component.scss']
 })
 export class RelatorioModalComponent {
-  diaSelecionadoInicio: string = ''; 
-  diaSelecionadoFim: string = ''; 
+  abastecimentos: Abastecimento[] = [];
+  diaSelecionadoInicio: string = '';
+  diaSelecionadoFim: string = '';
   tanqueSelecionado: string = '';
   formatoRelatorio: string = 'excel';
   bombaSelecionada: any = {
@@ -21,14 +25,22 @@ export class RelatorioModalComponent {
   };
   maxDate: string;
   tipoTanqueSelecionado: string = '';
- 
+
   constructor(
     public dialogRef: MatDialogRef<RelatorioModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private abastecimentoService: AbastecimentoService
+    private abastecimentoService: AbastecimentoService,
+    private snackBar: MatSnackBar
   ) {
     const today = new Date();
     this.maxDate = today.toISOString().split('T')[0];
+    this.carregarAbastecimentos();
+  }
+
+  carregarAbastecimentos() {
+    this.abastecimentoService.getAbastecimentos().subscribe(abastecimentos => {
+      this.abastecimentos = abastecimentos;
+    });
   }
 
   gerarRelatorio() {
@@ -37,13 +49,59 @@ export class RelatorioModalComponent {
     console.log('Data Final:', this.diaSelecionadoFim);
     console.log('Tanque Selecionado:', this.tanqueSelecionado);
     console.log('Bomba Selecionada:', this.bombaSelecionada);
-    
-    if (this.formatoRelatorio === 'excel') {
 
+    if (this.formatoRelatorio === 'excel') {
+      this.gerarRelatorioExcel();
     } else if (this.formatoRelatorio === 'pdf') {
-      
+      this.gerarRelatorioPDF();
     }
   }
+
+  gerarRelatorioExcel() {
+    const relatorioData = [
+    ];
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(relatorioData);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Relatorio');
+    XLSX.writeFile(wb, 'relatorio.xlsx');
+    this.showSuccessMessageAndCloseModal();
+  }
+
+  gerarRelatorioPDF() {
+    const doc = new jsPDF();
+    doc.setFontSize(8);
+
+    doc.text('Relatório de Abastecimento', 10, 10);
+    doc.text(`Data Inicial: ${this.diaSelecionadoInicio}`, 10, 20);
+    doc.text(`Data Final: ${this.diaSelecionadoFim}`, 10, 30);
+    doc.text(`Tanque Selecionado: ${this.tanqueSelecionado}`, 10, 40);
+    doc.text(`Bomba Selecionada: ${this.getSelectedBombas()}`, 10, 50);
+
+    let linha = 60;
+    this.abastecimentos.forEach(abastecimento => {
+      if (this.isBombaSelected(abastecimento.bomba)) {
+        // Converter a data de string para um objeto Date
+        const dataAbastecimento = new Date(abastecimento.data);
+        doc.text(`Data: ${this.formatDate(dataAbastecimento)}`, 10, linha); // Ajuste para o formato brasileiro
+        doc.text(`Combustível: ${abastecimento.combustivel}`, 50, linha);
+        doc.text(`Quantidade de Litros: ${abastecimento.quantidadeLitros}`, 100, linha);
+        doc.text(`Valor Abastecido: ${abastecimento.valorAbastecido}`, 150, linha);
+        linha += 10;
+      }
+    });
+
+    doc.save('relatorio.pdf');
+    this.showSuccessMessageAndCloseModal();
+}
+
+  formatDate(date: Date): string {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+
+
   onTipoCombustivelChange() {
     if (this.tanqueSelecionado === 'Gasolina') {
       this.tipoTanqueSelecionado = 'GasolinaTanque';
@@ -57,6 +115,55 @@ export class RelatorioModalComponent {
   }
 
   fecharModal() {
+    this.dialogRef.close();
+  }
+
+  resetModalFields() {
+    this.diaSelecionadoInicio = '';
+    this.diaSelecionadoFim = '';
+    this.tanqueSelecionado = '';
+    this.bombaSelecionada = {
+      Gasolina1: false,
+      Gasolina2: false,
+      Diesel1: false,
+      Diesel2: false
+    };
+    this.formatoRelatorio = 'excel';
+  }
+
+  private isBombaSelected(bomba: string) {
+    const bombaKey = this.getBombaKey(bomba);
+    return this.bombaSelecionada[bombaKey];
+  }
+
+  private getBombaKey(bomba: string) {
+    switch (bomba) {
+      case 'Bomba 1':
+        return 'Gasolina1';
+      case 'Bomba 2':
+        return 'Gasolina2';
+      case 'Bomba 3':
+        return 'Diesel1';
+      case 'Bomba 4':
+        return 'Diesel2';
+      default:
+        return '';
+    }
+  }
+
+  private getSelectedBombas() {
+    const selectedBombas = [];
+    if (this.bombaSelecionada.Gasolina1) selectedBombas.push('Bomba 1');
+    if (this.bombaSelecionada.Gasolina2) selectedBombas.push('Bomba 2');
+    if (this.bombaSelecionada.Diesel1) selectedBombas.push('Bomba 3');
+    if (this.bombaSelecionada.Diesel2) selectedBombas.push('Bomba 4');
+    return selectedBombas.join(', ');
+  }
+
+  private showSuccessMessageAndCloseModal() {
+    this.snackBar.open('Relatório salvo com sucesso!', 'Fechar', {
+      duration: 3000
+    });
     this.dialogRef.close();
   }
 }
