685d8b253c7cb4f5443560da07d698aaded7028b
Rodrigo Pablo <rpablo30@hotmail.com>
2023-10-29 14:10:56 -0300

    Feito para evitar data posterior a data de hoje.

--- src/app/modal/relatorioModal/relatorioModal.component.ts
+++ src/app/modal/relatorioModal/relatorioModal.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Abastecimento } from 'src/app/model/Abastecimeto.model';
+import { AbastecimentoService } from 'src/app/services/abastecimento.service';
 
 @Component({
   selector: 'app-relatorioModal',
@@ -9,18 +11,63 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class RelatorioModalComponent {
   diaSelecionado: string = '';
   tanqueSelecionado: string = '';
-  bombaSelecionada: string = '';
   diaSelecionadoInicio: string = ''; 
   diaSelecionadoFim: string = ''; 
   tipoTanqueSelecionado: string = '';
-
+  abastecimentos: Abastecimento[] = [];
+  bombaSelecionada: any = {
+    Gasolina1: false,
+    Gasolina2: false,
+    Diesel1: false,
+    Diesel2: false
+  };
+  maxDate: string;
 
   constructor(
     public dialogRef: MatDialogRef<RelatorioModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private abastecimentoService: AbastecimentoService
+  ) {
+    const today = new Date();
+    this.maxDate = today.toISOString().split('T')[0];
+  }
 
   gerarRelatorio() {
+    console.log('Data Inicial:', this.diaSelecionadoInicio);
+    console.log('Data Final:', this.diaSelecionadoFim);
+    console.log('Tanque Selecionado:', this.tanqueSelecionado);
+    console.log('Bomba Selecionada:', this.bombaSelecionada);
+    
+    // Obtenha a lista completa de abastecimentos
+    this.abastecimentoService.getAbastecimentos().subscribe((abastecimentos: Abastecimento[]) => {
+      console.log('Abastecimentos no Banco de Dados:', abastecimentos);
+      
+      // Aplique as seleções do modal para filtrar os abastecimentos
+      const abastecimentosFiltrados = this.filtrarAbastecimentos(abastecimentos);
+      
+      // Exiba os abastecimentos filtrados no console
+      console.log('Abastecimentos Filtrados:', abastecimentosFiltrados);
+    });
+}
+
+
+  filtrarAbastecimentos(abastecimentos: Abastecimento[]): Abastecimento[] {
+
+    return abastecimentos.filter((abastecimento) => {
+      const dataAbastecimento = new Date(abastecimento.data);
+      const dataInicial = new Date(this.diaSelecionadoInicio);
+      const dataFinal = new Date(this.diaSelecionadoFim);
+
+      if (
+        (dataAbastecimento >= dataInicial && dataAbastecimento <= dataFinal) && // Filtra por data
+        (this.tanqueSelecionado === abastecimento.combustivel) && // Filtra por tipo de combustível
+        (this.bombaSelecionada[abastecimento.bomba]) // Filtra por bombas
+      ) {
+        return true;
+      }
+
+      return false;
+    });
   }
 
   confirmarSelecoes() {
@@ -32,9 +79,7 @@ export class RelatorioModalComponent {
   }
 
   onTipoCombustivelChange() {
-   
     if (this.tanqueSelecionado === 'Gasolina') {
-   
       this.tipoTanqueSelecionado = 'GasolinaTanque';
     } else if (this.tanqueSelecionado === 'Diesel') {
       this.tipoTanqueSelecionado = 'DieselTanque';
@@ -44,10 +89,6 @@ export class RelatorioModalComponent {
       this.tipoTanqueSelecionado = '';
     }
   }
-  
-  
-
-
 
   fecharModal() {
     this.dialogRef.close();
